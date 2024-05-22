e5e7740058966aaa220cb580eea43250bac08a12
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-22 17:32:49 -0300

    Last update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Cadastre seu endereço:</h1>
-      <p>Preencha os campos para podermos enviar seus produtos.</p>
-    </div>
-  );
+function App(){
+  const [input, setInput] = useState('')
+  const [cadastro, setCadastro] = useState([]);
+
+
+  useEffect(()=>{
+    localStorage.setItem('@cadastro', JSON.stringify(cadastro))
+  }, [cadastro]);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setCadastro([...cadastro, input]);
+    setInput('');
+
+  }
+
+    return(
+        <div>
+            <h1>Cadastre seu endereço:</h1>
+
+            <form onSubmit={handleRegister}>
+                <label>Endereço:</label> <br/>
+                <input 
+                placeholder="Digite seu endereço"
+                value={input}
+                onChange={ (e) => setInput(e.target.value)  }
+                /><br /> <br />
+
+                <button type="submit">Cadastrar</button>
+
+
+            </form>
+         
+          <br /> <br />
+          
+          <ul>
+            {
+              cadastro.map( cadastro => (
+                <li key={cadastro}>{cadastro}</li>
+              ))
+            }
+          </ul>
+
+        </div>
+    )
 }
 
 export default App;
